61b30220a3bba0c78f41a1473407523ad1f8d9ab
snow-fox <taijunjet@hotmail.com>
2023-01-09 01:28:27 +0000

    remove double wrap

--- controllers/pid.pyx
+++ controllers/pid.pyx
@@ -6,12 +6,3 @@ cdef class PyPID:
         del self.thisptr
     def get(self):
         return self.thisptr.get()
-
-cdef class PID:
-    cdef PyPID* thisptr  # hold a Python instance which we're wrapping
-    def __cinit__(self):
-        self.thisptr = new PyPID()
-    def __dealloc__(self):
-        del self.thisptr
-    def get(self):
-        return self.thisptr.get()
